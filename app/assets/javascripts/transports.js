95f13f643046c4df18e3f2b9e49dae649cb36b54
Runar Flåten <runarflaaten@gmail.com>
2015-03-02 11:31:31 +0100

    Got jquery working aya

--- app/assets/javascripts/transports.js
+++ app/assets/javascripts/transports.js
@@ -1,3 +1,4 @@
+/*
 function initialize() {
     var mapOptions = {
 	center: { lat: -34.397, lng: 150.644 },
@@ -8,3 +9,35 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+*/
+/*
+var map;
+var geocoder;
+var bounds = new google.maps.LanLngBounds();
+var markersArray = [];
+
+var origin;
+var destination;
+
+function initialize() {
+    var service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix(
+	{
+	    origins: [origin],
+	    destinations: [destination],
+	    travelMode: google.maps.TravelMode.DRIVING,
+	    unitSystem: google.maps.UnitSystem.METRIC,
+	    avoidHighways: false,
+	    avoidTolls: false
+	}, callback);
+}*/
+
+$(document).ready(function() {
+    $("button").click(function () {
+	origin = $("#from").val();
+	destination = $("#to").val();
+	$("#randomshit").append(origin);
+	$("#randomshit").append(destination);
+    });
+});
+		   
