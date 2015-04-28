4c767302d3ee3bab22df3e5f9dd0bb54d08e2e5a
Runar Flåten <runarflaaten@gmail.com>
2015-04-28 23:03:54 +0200

    Added timing feature and tried to remove storage of cache

--- lib/ruter.rb
+++ lib/ruter.rb
@@ -1,15 +1,22 @@
+require "benchmark"
+
 class Ruter
   include HTTParty
 
   base_uri 'reisapi.ruter.no'
 
   def self.getRoute(from, to)
-    response = get('/Travel/GetTravels/', query: {
-                                            fromplace: from,
-                                            toplace: to,
-                                            isAfter: true,
-                                        })
-end
+    response = nil
+    time = Benchmark.realtime do
+      response = get('/Travel/GetTravels/', query: {
+                                              fromplace: from,
+                                              toplace: to,
+                                              isAfter: true,
+                                          })
+    end
+    puts "Time elapsed #{time*1000} milliseconds"
+    return response
+  end
 
   def self.getPlaceWithName(place)
 
